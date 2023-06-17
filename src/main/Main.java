e60f6ff73e736c972a30c953652d4854014828f7
MykolaR2004 <132054530+MykolaR2004@users.noreply.github.com>
2023-06-17 20:35:00 +0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -50,7 +50,6 @@ public class Main {
         for (int i = 0 ; i< parts.length-1; i++){
             for (String act : acts) {
                 if ((parts[i].equals(act) || parts[i].equals("-")) && (parts[i+1].equals("+") || parts[i+1].equals("*") || parts[i+1].equals("/"))) errorsAmount++;
-                if (parts[i].equals("=") && parts[i+1].equals("-")) errorsAmount++;
             }
         }
         return errorsAmount;
